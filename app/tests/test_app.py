6f3632e8771a18e4971020d11cf64194b8619098
JesusAIM64 <jesus64ibarra@gmail.com>
2025-11-05 13:44:13 -0600

    Update test_app.py

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -42,4 +42,4 @@ def test_valid_email():
 def test_health_check(client):
     """Test básico de salud de la aplicación"""
     response = client.get('/login')
-    assert response.status_code == 200
+    assert response.status_code == 200 response.status_code == 200
